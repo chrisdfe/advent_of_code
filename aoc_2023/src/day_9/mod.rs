a367fc594ad8d53cc338d3a9c75a694ab0c1f217
chrisdfe <christopher.d.ferris@gmail.com>
2023-12-23 11:15:46 -0800

    Day 9 part 2

--- aoc_2023/src/day_9/mod.rs
+++ aoc_2023/src/day_9/mod.rs
@@ -32,6 +32,25 @@ fn find_next_value(numbers: &Vec<i32>) -> i32 {
   result
 }
 
+fn find_previous_value(numbers: &Vec<i32>) -> i32 {
+  let differences = tuple_vec_from_vec(&numbers)
+    .into_iter()
+    .map(|(a, b)| b - a)
+    .collect::<Vec<_>>();
+
+  let differences_are_zero = differences.iter().find(|num| **num != 0) == None;
+
+  let previous_below_value = if differences_are_zero {
+    0
+  } else {
+    find_previous_value(&differences)
+  };
+
+  let result = numbers.first().unwrap() - previous_below_value;
+  println!("result: {}", result);
+  result
+}
+
 fn part_1(input: &str) -> i32 {
   input
     .split('\n')
@@ -46,6 +65,20 @@ fn part_1(input: &str) -> i32 {
     .unwrap()
 }
 
+fn part_2(input: &str) -> i32 {
+  input
+    .split('\n')
+    .map(|line| {
+      line
+        .split(' ')
+        .map(|num_string| num_string.parse::<i32>().unwrap())
+        .collect::<Vec<_>>()
+    })
+    .map(|numbers| find_previous_value(&numbers))
+    .reduce(|acc, result| acc + result)
+    .unwrap()
+}
+
 pub fn run() -> Result<(), std::io::Error> {
   println!("running day 9");
   let input = crate::utils::read_input(INPUT_FILENAME)?;
@@ -53,8 +86,8 @@ pub fn run() -> Result<(), std::io::Error> {
   let part_1_total = part_1(&input);
   println!("part_1 total {}", part_1_total);
 
-  // let part_2_total = part_2(&input);
-  // println!("part_2 total {}", part_2_total);
+  let part_2_total = part_2(&input);
+  println!("part_2 total {}", part_2_total);
 
   Ok(())
 }
@@ -63,7 +96,7 @@ pub fn run() -> Result<(), std::io::Error> {
 mod tests {
   use crate::utils::read_input;
 
-  use super::{part_1, INPUT_FILENAME};
+  use super::{part_1, part_2, INPUT_FILENAME};
 
   const EXAMPLE_INPUT_FILENAME: &str = "./src/day_9/example_input.txt";
 
@@ -74,27 +107,24 @@ mod tests {
     assert_eq!(result, 114);
   }
 
-  // #[test]
-  // pub fn day_9_part_1_solution_works() {
-  //   let contents = read_input(INPUT_FILENAME).unwrap();
-  //   let result = part_1(&contents);
-  //   assert_eq!(result, 12737);
-  // }
+  #[test]
+  pub fn day_9_part_1_solution_works() {
+    let contents = read_input(INPUT_FILENAME).unwrap();
+    let result = part_1(&contents);
+    assert_eq!(result, 2043183816);
+  }
 
-  // #[test]
-  // pub fn day_9_part_2_example_works() {
-  //   let contents = read_input(PART_2_EXAMPLE_INPUT_FILENAME).unwrap();
-  //   let result = part_2(&contents);
-  //   assert_eq!(result, 6);
-  // }
+  #[test]
+  pub fn day_9_part_2_example_works() {
+    let contents = read_input(EXAMPLE_INPUT_FILENAME).unwrap();
+    let result = part_2(&contents);
+    assert_eq!(result, 2);
+  }
 
-  // #[test]
-  // pub fn day_9_part_2_solution_works() {
-  //   use more_asserts::assert_gt;
-
-  //   let contents = read_input(INPUT_FILENAME).unwrap();
-  //   let result = part_2(&contents);
-  //   assert_gt!(result, 21409);
-  //   assert_eq!(result, 9064949303801);
-  // }
+  #[test]
+  pub fn day_9_part_2_solution_works() {
+    let contents = read_input(INPUT_FILENAME).unwrap();
+    let result = part_2(&contents);
+    assert_eq!(result, 1118);
+  }
 }
